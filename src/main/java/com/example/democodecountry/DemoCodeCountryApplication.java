8c3be198b878d257dfa115615987fa55becdd67b
Admin <carolyn.sher@wgu.edu>
2023-08-31 07:36:26 -0700

    Initial Commit

---
+++ src/main/java/com/example/democodecountry/DemoCodeCountryApplication.java
@@ -0,0 +1,13 @@
+package com.example.democodecountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCodeCountryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCodeCountryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/democodecountry/DemoCodeCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democodecountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCodeCountryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
